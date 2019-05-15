1387a640f1d8ba8da7dbac131ff68ddc78701788
severedwig <50678610+severedwig@users.noreply.github.com>
2019-05-15 18:34:43 -0500

    Update minimos_linea_recta.java

--- minimos_linea_recta.java
+++ minimos_linea_recta.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.text.DecimalFormat;
 
 public class minimos_linea_recta{
 	public static void main(String[] args){
@@ -6,11 +7,13 @@ public class minimos_linea_recta{
 		
 		/*AQUI SE GENERAN DE MANERA ALEATORIA LOS VALORES DE X & Y PARA EL PROBLEMA A RESOLVER*/
 		//n.- ES EL NUMERO DE VALORES DADOS A "X"
-		int limite = 6,n,x,y;
-		n = (int)(Math.random()*limite)+1;
+		int limite = 5,n,x,y;
+		n = (int)(Math.random()*limite)+2;
 		System.out.println("n = "+n);
 		/////////////////////////////////////////////////
 		Random r = new Random();
+
+		DecimalFormat round = new DecimalFormat ("0.00000000");
 
 		double random,
 		randomx,
@@ -26,23 +29,30 @@ public class minimos_linea_recta{
 
 		for(x=0;x<n;x++){
 			for(y=0;y<1;y++){
-				valores[x][y] = random = min + r.nextDouble() * (max - min);
+				random = min + r.nextDouble() * (max - min);
+				valores[x][y] = Double.parseDouble(round.format(random));
 			}
-			valores[x][y] = random = min + r.nextDouble() * (max - min);
+			random = min + r.nextDouble() * (max - min);
+			valores[x][y] = Double.parseDouble(round.format(random));
 			System.out.println("["+x+"]["+(y-1)+"]= "+valores[x][y-1]+"\t["+x+"]["+y+"]= "+valores[x][y]);
 			//SE OBTIENE LA SUMATORIA DE LA MULTIPLICACION DE XY
 			sumXY += valores[x][y-1]*valores[x][y];
+			sumXY = Double.parseDouble(round.format(sumXY));
 		}
 
 		/*SE OBTIENEN LOS DATOS DE LAS SUMATORIAS DEACUERDO AL PROBLEMA GENERADO*/
 		for(x=0;x<n;x++){
 			//SUMATORIA DE X
 			sumX+=valores[x][0];
+			sumX = Double.parseDouble(round.format(sumX));
 			//SUMATORIA DE Y
 			sumY+=valores[x][1];
+			sumY = Double.parseDouble(round.format(sumY));
+			//SUMATORIA DE X^2
+			valores[x][0] = Double.parseDouble(round.format(valores[x][0]));
+			double sumXCuadradoAuxiliar = Double.parseDouble(round.format(Math.pow(valores[x][0],2)));
+			sumXCuadrado += sumXCuadradoAuxiliar;
 		}
-		//SUMATORIA DE X^2
-		sumXCuadrado = sumX*sumX;
 		System.out.println("\nSumatoria X="+sumX
 			+"\nSumatoria X^2="+sumXCuadrado
 			+"\nSumatoria Y="+sumY
@@ -54,11 +64,22 @@ public class minimos_linea_recta{
 			+"1)\t"+n+"a0\t"+sumX+"a1\t=\t"+sumY
 			+"\n2)\t"+sumX+"a0\t"+sumXCuadrado+"a1\t=\t"+sumXY);
 		double det=(n*sumXCuadrado)-(sumX*sumX);
+		det = Double.parseDouble(round.format(det));
+
 		double a0 = ((sumY*sumXCuadrado)-(sumX*sumXY))/det;
-        	double a1 = ((n*sumXY)-(sumY*sumX))/det;
+		a0 = Double.parseDouble(round.format(a0));
+
+        double a1 = ((n*sumXY)-(sumY*sumX))/det;
+        a1 = Double.parseDouble(round.format(a1));
 
         System.out.println("\n"
         	+"a0="+a0
-        	+"\na1="+a1);
+        	+"\na1="+a1+"\n\n");
+
+        for(int i=0;i<n;i++){
+        	double gx = a0 + a1*valores[i][0];
+        	gx = Double.parseDouble(round.format(gx));
+        	System.out.println(a0+" + "+a1+"("+valores[i][0]+") = "+gx);
+        }
 	}
 }
